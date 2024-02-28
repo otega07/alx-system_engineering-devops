f40333f9620966fc779415ed0f5e6122d4478843
otega07 <okiriotega@gmail.com>
2024-02-28 18:19:10 +0100

    advanced third check

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).map { |match| match.join(':') }.join(", ")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).map { |match| match.join(":") }.join(", ")
