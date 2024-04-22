06e356589ec6fc17d3ee701c1beba43609a4c8c0
otega07 <okiriotega@gmail.com>
2024-04-22 15:12:52 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,10 @@
 # Manifest to install Flask (2.1.0) from pip3
-
-# Define a class for managing Flask installation
-class mymodule::flask_install {
-    package { 'flask':
-        ensure   => '2.1.0',
-        provider => 'pip3',
-    }
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
 
-# Define main manifest
-class { 'mymodule::flask_install': }
+package {'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
+}
