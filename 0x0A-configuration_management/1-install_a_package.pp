cad7c042964c6e54354cfaf0ba6a7d99c2c621de
otega07 <okiriotega@gmail.com>
2024-04-22 14:07:38 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Install flask (2.1.0) from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask (2.1.0) using pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
