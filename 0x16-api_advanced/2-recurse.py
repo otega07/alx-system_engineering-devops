2017d8848cc43ddfe5d5000e0c8b68b84686a77b
otega07 <okiriotega@gmail.com>
2024-06-23 22:27:55 +0100

    recursive

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,9 +5,10 @@ Python script that queries a list of all hot posts on a given Reddit subreddit.
 
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None, count=0):
     """
-    Recursively retrieves a list of titles of all hot posts
+    Recursively retrieves a list of titles of all hot posts.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
@@ -18,16 +19,18 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
+
     if response.status_code == 200:
         data = response.json().get("data")
         after = data.get("after")
         count += data.get("dist")
-        
+
         for child in data.get("children"):
             hot_list.append(child.get("data").get("title"))
-        
+
         if after:
             return recurse(subreddit, hot_list, after, count)
         else:
@@ -35,10 +38,11 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
     else:
         return None
 
+
 if __name__ == "__main__":
     subreddit = input("Enter a subreddit: ").strip()
     hot_posts = recurse(subreddit)
-    
+
     if hot_posts is None:
         print(f"Subreddit '{subreddit}' not found or invalid.")
     else:
