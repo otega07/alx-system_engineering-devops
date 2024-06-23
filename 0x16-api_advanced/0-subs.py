57609ef9c05a524d2280ac513daf8d6e9719c83c
otega07 <okiriotega@gmail.com>
2024-06-23 19:23:18 +0100

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that queries the Reddit API for the number of subscribers
+Python script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
@@ -9,11 +9,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Chrome/5.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribersi']
+        subscribers = data['data']['subscribers']
         return subscribers
-    else:
+    elif response.status_code == 404:
         return 0
+    else:
+        return -1
