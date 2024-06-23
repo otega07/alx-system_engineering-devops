87746598cc944bee2df15c79c3b72b337603309e
otega07 <okiriotega@gmail.com>
2024-06-23 19:47:30 +0100

    sub 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Python script that queries subscribers on a given Reddit subreddit.
-"""
-
+""" Python scipt that query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    elif response.status_code == 404:
-        print(f"The subreddit r/{subreddit} does not exist.")
+    if response.status_code == 404:
         return 0
-    else:
-        print(f"Failed to retrieve data for subreddit r/{subreddit}. Status code: {response.status_code}")
-        return -1
+    results = response.json().get("data")
+    return results.get("subscribers")
