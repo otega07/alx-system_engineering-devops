38895539b062e4b10b50b190aa52b194030fa27b
otega07 <okiriotega@gmail.com>
2024-06-23 18:17:37 +0100

    subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
+''' 
+Python script that gather and display employee data using REST API
 '''
-Python script that gathers and display employee task data from API
-'''
-
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     if len(argv) > 1:
         user_id = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-
-        try:
-            user_response = requests.get(f"{url}users/{user_id}")
-            user_response.raise_for_status()
-            user = user_response.json()
-            name = user.get("name")
-            if name:
-                tasks_response = requests.get(f"{url}todos?userId={user_id}")
-                tasks = tasks_response.json()
-                completed_tasks = [task for task in tasks if task.get("completed")]
-                print(f"Employee {name} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
-                for task in completed_tasks:
-                    print(f"\t {task.get('title')}")
-        except requests.exceptions.HTTPError:
-            print("User not found")
+        base_url = "https://jsonplaceholder.typicode.com/"
+        
+        user_response = requests.get(f"{base_url}users/{user_id}")
+        user_name = user_response.json().get("name")
+        
+        if user_name:
+            tasks_response = requests.get(f"{base_url}todos?userId={user_id}")
+            tasks = tasks_response.json()
+            completed_tasks = [task for task in tasks if task.get("completed")]
+            
+            print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
+            for task in completed_tasks:
+                print(f"\t {task.get('title')}")
