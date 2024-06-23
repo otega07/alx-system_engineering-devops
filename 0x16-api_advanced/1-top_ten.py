ce36058ddd52d81310ddc27c56de61a6cd87913d
otega07 <okiriotega@gmail.com>
2024-06-23 23:13:05 +0100

    topten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Function to print hot posts on a given Reddit subreddit."""
+
+
 import requests
 
 
@@ -7,13 +9,16 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
     try:
         results = response.json().get("data")
         children = results.get("children")
@@ -22,7 +27,7 @@ def top_ten(subreddit):
         else:
             for post in children:
                 print(post.get("data").get("title"))
-    except (requests.exceptions.JSONDecodeError, AttributeError):
+    except ValueError:
         print("None")
 
 
