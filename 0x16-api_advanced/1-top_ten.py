0b6e07f7973fd5a0e60e84633be35250900ceaf1
otega07 <okiriotega@gmail.com>
2024-06-23 23:20:29 +0100

    topten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-'''
-module contains the function top_ten
-'''
+"""
+This module contains the function top_ten
+"""
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """
+    Returns the top ten hot posts for a given subreddit
+    """
+    user = {'User-Agent': 'python:top_ten:v1.0 (by /u/yourusername)'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    response = requests.get(url, headers=user, allow_redirects=False)
+    
+    if response.status_code != 200:
+        print(None)
+        return
+    
     try:
-        for post in url.get('data').get('children'):
+        data = response.json().get('data')
+        for post in data.get('children'):
             print(post.get('data').get('title'))
     except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(argv) > 1:
+        top_ten(argv[1])
