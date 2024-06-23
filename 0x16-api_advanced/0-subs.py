3d9db6906cca8a9610ab5140eba503f9f03262e2
otega07 <okiriotega@gmail.com>
2024-06-23 20:06:11 +0100

    sub 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        data = response.json().get("data", {})
+        return dataa.get("subscribers", 0)
+    elif response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    else:
+        return 0
+
+    # results = response.json().get("data")
+    # return results.get("subscribers")
